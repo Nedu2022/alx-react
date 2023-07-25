cf9e15c9a317ad27667804992d170d5020fd0651
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-07-25 03:16:47 +0100

    Basic setup

--- 0x00-Webpack/task_0/dist/index.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-$(document).ready(function () {
-  $('body').append(
-    '<p>Holberton Dashboard</p>' +
-      '<p>Dashboard data for the students</p>' +
-      '<p>Copyright - Holberton School</p>'
-  );
-});
